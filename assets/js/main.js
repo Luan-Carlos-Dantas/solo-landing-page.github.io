3476a936ea65ddab45f95465cd94a5fc98271030
Luan-Carlos-Dantas <luancarlos492@gmail.com>
2022-10-16 18:36:17 -0300

    Finishing mobile devices

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,74 @@
+
+window.addEventListener('scroll', onScroll)
+onScroll()
+function onScroll(){
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section){
+    const targetLine = scrollY + (innerHeight / 2)
+
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+
+    const sectionEndAt = sectionTop + sectionHeight
+
+    const sectionTopReachOrPassedTargetLine =  targetLine >= sectionTop
+    const sectionEndOrPassedTargetLine =  sectionEndAt <= targetLine  
+
+    const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndOrPassedTargetLine
+
+    const currentSection = section.getAttribute('id')
+
+
+    const menuElement = document.querySelector(`.menu a[href*=${currentSection}]`)
+
+    menuElement.classList.remove('active')
+
+    if(sectionBoundaries){
+        menuElement.classList.add('active')
+    }
+}
+
+// Animation Menu Hamburguer
+window.addEventListener('onload', ()=>{
+    if(body.classList.contains('show-menu')){
+        body.classList.remove('show-menu')
+    }
+})
+
+var forEach=function(t,o,r){if("[object Object]"===Object.prototype.toString.call(t))for(var c in t)Object.prototype.hasOwnProperty.call(t,c)&&o.call(r,t[c],c,t);else for(var e=0,l=t.length;l>e;e++)o.call(r,t[e],e,t)};
+var hamburgers = document.querySelectorAll(".hamburger");
+var body = document.querySelector('body')
+    if (hamburgers.length > 0) {
+      forEach(hamburgers, function(hamburger) {
+        hamburger.addEventListener("click", function() {
+          this.classList.toggle("is-active");
+        }, false);
+      });
+}
+
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function() {
+    if(!body.classList.contains('show-menu')){
+        body.classList.add('show-menu')
+    }else{
+        body.classList.remove('show-menu')
+    }
+})
+
+function closeMenu(){
+    if(body.classList.contains('show-menu')){
+        body.classList.remove('show-menu')
+        hamburger.classList.toggle("is-active");
+    };
+}
+
+
+// Testimonials slide
 const swiper = new Swiper('.swiper',{
     loop: true,
     slidesPerView: 1,
